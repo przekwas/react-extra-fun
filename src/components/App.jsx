00a05aa92f66d8d37fbebea5f17f8dedf87768a2
Luke Przekwas <galactica@The-Galactica.local>
2018-01-24 09:57:06 -0600

    step 7 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,11 +8,26 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: "I'm having fun with React!"
+            text: "I'm having fun with React!",
+            newInput: ""
         }
     }
+
+    handleInput(event) {
+        this.setState({newInput: event.target.value});
+    }
+
     render() {
-        return <h1>{this.props.message} {this.state.text}</h1>
+        return (
+            <React.Fragment>
+                <h1>{this.props.message} {this.state.text}</h1>
+                <input
+                    type="text"
+                    placeholder={this.state.text}
+                    onChange={(event) => { this.handleInput(event) }}
+                />
+            </React.Fragment>
+        )
     }
 
 }
